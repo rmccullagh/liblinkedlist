1b27ba5963bccc66a26237d4a05acff58490289e
Ryan McCullagh <me@ryanmccullagh.com>
2016-01-29 20:07:52 -0600

    add license

--- LinkedList.h
+++ LinkedList.h
@@ -1,6 +1,20 @@
 #ifndef OBJECT_LINKED_LIST_H
 #define OBJECT_LINKED_LIST_H
 
+/**
+ * Copyright (c) 2016 Ryan McCullagh <me@ryanmccullagh.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stddef.h>
 #include <object.h>
 
--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2016 Ryan McCullagh <me@ryanmccullagh.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <object.h>
@@ -15,15 +29,13 @@ int main(void)
 	LinkedList ll;
 	LinkedList_INIT(&ll, dtor);
 
-	
 	LinkedList_ADD(&ll, newString("McCullagh"));
 	LinkedList_ADD(&ll, newString("Ryan"));
 
-
 	LinkedListNode *head = LL_CHAIN(&ll);
 
-	while(head != NULL) {
-
+	while(head != NULL) 
+	{
 		OBJECT_DUMP((Object *)(LLN_VALUE(head)));
 
 		head = LLN_NEXT(head);
@@ -31,4 +43,3 @@ int main(void)
 
 	return 0;
 }
-
